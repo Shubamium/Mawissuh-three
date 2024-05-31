8201596a645e6be16c2d29132447d96d03ff5655
Vincent_Liussyaputra <liusvinv@gmail.com>
2024-05-31 23:20:55 +0700

    ADd heading

--- src/app/components/layouts/header/Header.tsx
+++ src/app/components/layouts/header/Header.tsx
@@ -1,50 +1,74 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 import "./header.scss";
 import { FaTwitter } from "react-icons/fa";
-import { BiMenu } from "react-icons/bi";
+import { BiBriefcase, BiHome, BiHomeAlt2, BiMenu } from "react-icons/bi";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import {
+  BsArrowLeftShort,
+  BsHouse,
+  BsHouseDown,
+  BsHouseFill,
+  BsStars,
+} from "react-icons/bs";
+import { FcHome } from "react-icons/fc";
+import { GrHome, GrHomeRounded } from "react-icons/gr";
+import { createPortal } from "react-dom";
+import { RiCloseCircleFill } from "react-icons/ri";
+import { AiFillCloseCircle } from "react-icons/ai";
+import { TbArrowBarToUp } from "react-icons/tb";
+import { FaComputer } from "react-icons/fa6";
+import { CgCode } from "react-icons/cg";
+import { GiPenguin } from "react-icons/gi";
+
 type Props = {};
 
 export default function Header({}: Props) {
   const path = usePathname();
+  const [menuOpen, setMenuOpen] = React.useState(false);
+  const [init, setInit] = React.useState(false);
 
   const include = (paths: string) => {
     return path.includes(paths);
   };
+
+  useEffect(() => {
+    setInit(true);
+  }, []);
   return (
     <header id="header">
       <h1 hidden={true}>Marimanagement</h1>
-      <nav className="panel">
+      <nav className="panel desktop">
         <div className="box"></div>
         <Link className={`nav-btn ${path === "/" ? "active" : ""}`} href={"/"}>
-          home
+          <BsHouseFill /> home
         </Link>
         <Link
           className={`nav-btn ${include("management") ? "active" : ""}`}
           href={"/management"}
         >
-          management
+          <BiBriefcase /> management
         </Link>
         <Link
           className={`nav-btn ${include("talents") ? "active" : ""}`}
           href={"/talents"}
         >
-          talents
+          <BsStars /> talents
         </Link>
       </nav>
-      <div className="credits panel">
-        <a href="https://x.com/Shubamium2" className="credit">
+      <div className="credits panel credit desktop">
+        <a href="https://x.com/Shubamium2" target="_blank" className="credit">
           <h2>web design</h2>
-          <p>@shubamium</p>
+          <GiPenguin />
         </a>
       </div>
-      <div className="credits panel">
+      <div className="credits panel contact desktop">
         {/* <a href="https://x.com/Shubamium2" className="credit">
           <h2>web design</h2>
           <p>@shubamium</p>
         </a> */}
+
         <div className="contacts">
           <a
             href="https://x.com/SheepishMari"
@@ -69,6 +93,71 @@ export default function Header({}: Props) {
           </button>
         )}
       </div>
+
+      <div className="credits panel hidden-nav">
+        <button
+          className="taskbar-btn btn"
+          onClick={() => {
+            setMenuOpen(true);
+          }}
+        >
+          <BiMenu />
+        </button>
+      </div>
+
+      {init &&
+        createPortal(
+          <div id="fs-menu" className={`${menuOpen ? "open" : "closed"}`}>
+            <nav>
+              <Link
+                className={`nav-btn ${path === "/" ? "active" : ""}`}
+                href={"/"}
+                onClick={() => {
+                  setMenuOpen(false);
+                }}
+              >
+                <BsHouseFill /> home
+              </Link>
+              <Link
+                className={`nav-btn ${include("management") ? "active" : ""}`}
+                href={"/management"}
+                onClick={() => {
+                  setMenuOpen(false);
+                }}
+              >
+                <BiBriefcase /> management
+              </Link>
+              <Link
+                className={`nav-btn ${include("talents") ? "active" : ""}`}
+                href={"/talents"}
+                onClick={() => {
+                  setMenuOpen(false);
+                }}
+              >
+                <BsStars /> talents
+              </Link>
+              <a
+                className="nav-btn"
+                onClick={() => {
+                  setMenuOpen(false);
+                }}
+              >
+                <TbArrowBarToUp /> close
+              </a>
+            </nav>
+
+            <a href="https://x.com/Shubamium2" className="credit">
+              <span className="title">
+                <CgCode /> web design
+              </span>
+              <span className="name">
+                shubamium
+                <GiPenguin />
+              </span>
+            </a>
+          </div>,
+          document.body
+        )}
     </header>
   );
 }
